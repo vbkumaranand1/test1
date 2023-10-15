6a3128997a141a108dcd1048cf9ed851b1119d2a
Anand <vbkumaranand@gmail.com>
2023-10-15 21:42:25 +0530

    Updated Branch

--- src/test/java/com/virtusa/ReadAndWriteData.java
+++ src/test/java/com/virtusa/ReadAndWriteData.java
@@ -3,32 +3,40 @@ package com.virtusa;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
-import java.io.File;
+
 import java.io.FileInputStream;
 import java.util.ArrayList;
 
-public class ReadAndWriteData {
+//public class ReadAndWriteData {
+    public class ReadAndWriteData {
 
     public static XSSFSheet workSheet;
     public static XSSFWorkbook workBook;
 
     @Test
-        public void readWriteData(){
-        //Read data from XLSX file- store in ArrayList and print to console log
-        ReadAndWriteData readAndWriteData=new ReadAndWriteData();
-        ArrayList<ArrayList<String>> retVal = readAndWriteData.getExcelData(System.getProperty("user.dir") + "\\src\\test\\resources\\ExcelData.xlsx", "Sheet1");
-        for(int i=0;i<retVal.size();i++){
-            ArrayList<String> arrVal=retVal.get(i);
-            for(int j=0;j<arrVal.size();j++){
-                if(j ==arrVal.size()-1){
-                    System.out.print(arrVal.get(j));
-                }else{
-                    System.out.print(arrVal.get(j)+", ");
+    public void readWriteData(){
+        try{
+            //Read data from XLSX file- store in ArrayList and print to console log
+            ReadAndWriteData readAndWriteData=new ReadAndWriteData();
+            ArrayList<ArrayList<String>> retVal = readAndWriteData.getExcelData(System.getProperty("user.dir") + "\\src\\test\\resources\\ExcelData.xlsx", "Sheet1");
+//            for(int i=0;i<retVal.size();i++){
+
+            for (ArrayList<String> arrVal : retVal) {
+                for (int j = 0; j < arrVal.size(); j++) {
+                    if (j == arrVal.size() - 1) {
+                        System.out.print(arrVal.get(j));
+                    } else {
+                        System.out.print(arrVal.get(j) + ", ");
+                    }
                 }
+                System.out.println();
             }
-            System.out.println();
+
+        }catch (Exception e){
+            Assertions.fail("readWriteData :- Exception :- "+e);
         }
 
     }
@@ -38,17 +46,20 @@ public class ReadAndWriteData {
         ArrayList<ArrayList<String>> objArrayList = null;
         try{
 //            ArrayList<ArrayList<String>> objArrayList=new ArrayList<ArrayList<String>>();
-            FileInputStream ExcelFile=new FileInputStream(new File(Path)) ;
+            FileInputStream ExcelFile=new FileInputStream(Path) ;
             workBook=new XSSFWorkbook(ExcelFile);
             workSheet=workBook.getSheet(SheetName);
 
             int numOfRows=workSheet.getLastRowNum();
             int numOfColumns = workSheet.getRow(0).getLastCellNum();
-            objArrayList= new ArrayList<ArrayList<String>>(numOfRows);
+//            objArrayList= new ArrayList<ArrayList<String>>(numOfRows);
+            objArrayList= new ArrayList<>(numOfRows);
 
 
             for(int i=0;i<=numOfRows;i++){
-                objArrayList.add(new ArrayList<String>());
+//                objArrayList.add(new ArrayList<String>());
+                objArrayList.add(new ArrayList<>());
+
                 for(int j=0;j<numOfColumns;j++){
                     String val;
                     if(workSheet.getRow(i).getCell(j).getCellType() == CellType.NUMERIC){
--- src/test/java/com/virtusa/SeleniumTests.java
+++ src/test/java/com/virtusa/SeleniumTests.java
@@ -2,8 +2,6 @@ package com.virtusa;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-//import org.junit.Assert;
-//import org.junit.Test;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
@@ -28,138 +26,108 @@ public class SeleniumTests {
 
         @Test
         public void launchBrowserVerifyLogoDisplayedTestCase(){
-            System.out.println("browser :- "+browser);
-            System.out.println("url :- "+url);
+            try{
+                System.out.println("browser :- "+browser);
+                System.out.println("url :- "+url);
 
-            switch(browser){
-                case "chrome":
-                    ChromeOptions chromeOptions=new ChromeOptions();
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver(chromeOptions);
-                    break;
-                case "edge":
-                    EdgeOptions options = new EdgeOptions();
-                    options.addArguments("--remote-allow-origins=*");
-                    WebDriverManager.edgedriver().setup();
-                    driver = new EdgeDriver(options);
-                    break;
-                default:
-                    Assertions.fail("Provide correct Browser name(chrome/edge) to execute, currently provided browser name is :- "+browser);
-//                Assert.fail("Provide correct Browser name(chrome/edge) to execute, currently provided browser name is :- "+browser);
+                switch(browser){
+                    case "chrome":
+                        ChromeOptions chromeOptions=new ChromeOptions();
+                        WebDriverManager.chromedriver().setup();
+                        driver = new ChromeDriver(chromeOptions);
+                        break;
+                    case "edge":
+                        EdgeOptions options = new EdgeOptions();
+                        options.addArguments("--remote-allow-origins=*");
+                        WebDriverManager.edgedriver().setup();
+                        driver = new EdgeDriver(options);
+                        break;
+                    default:
+                        Assertions.fail("Provide correct Browser name(chrome/edge) to execute, currently provided browser name is :- "+browser);
+                }
+
+
+
+                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+                driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(190));
+                driver.get(url);
+                //Close cookie accept
+                if(driver.findElement(By.xpath("//button[@id='onetrust-accept-btn-handler']")).isDisplayed()){
+                    System.out.println("Clicked cookie accept");
+                    driver.findElement(By.xpath("//button[@id='onetrust-accept-btn-handler']")).click();
+                    driver.manage().window().maximize();
+
+                }else{
+                    System.out.println("Not displayed Clicked cookie accept");
+                }
+
+                boolean blnStatus = driver.findElement(By.xpath("//img[@class='coh-image coh-image-responsive-xl' and contains(@alt,'Automation Anywhere')]")).isDisplayed();
+                if(blnStatus){
+                    System.out.println("Able to see the automationanywhere Logo image is displayed");
+                }else{
+                    System.out.println("Not Able to see the automationanywhere Logo image  displayed");
+                }
+                Assertions.assertTrue(blnStatus,"Logo is not displayed");
+
+                //Logo is displayed
+                blnStatus =driver.findElement(By.xpath("//a[@title='Request Demo' and contains(@class,'coh-link utility-nav-link coh-style-solid-orange-button')]")).isEnabled();
+                if(blnStatus){
+                    System.out.println("Able to see the automationanywhere 'Request Demo' button enabled");
+                }else{
+                    System.out.println("Not Able to see the automationanywhere 'Request Demo' button not enabled");
+                }
+                Assertions.assertTrue(blnStatus,"Logo is not displayed");
+            }catch (Exception e){
+                Assertions.fail("launchBrowserVerifyLogoDisplayedTestCase :_ Exception :- "+e);
             }
-
-
-
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(190));
-        driver.get(url);
-        //Close cookie accept
-        if(driver.findElement(By.xpath("//button[@id='onetrust-accept-btn-handler']")).isDisplayed()){
-            System.out.println("Clicked cookie accept");
-            driver.findElement(By.xpath("//button[@id='onetrust-accept-btn-handler']")).click();
-            driver.manage().window().maximize();
-
-        }else{
-            System.out.println("Not displayed Clicked cookie accept");
-        }
-
-       boolean blnStatus = driver.findElement(By.xpath("//img[@class='coh-image coh-image-responsive-xl' and contains(@alt,'Automation Anywhere')]")).isDisplayed();
-       if(blnStatus){
-           System.out.println("Able to see the automationanywhere Logo image is displayed");
-       }else{
-           System.out.println("Not Able to see the automationanywhere Logo image  displayed");
-       }
-//       Assert.assertTrue("Logo is not displayed",blnStatus);
-       Assertions.assertTrue(blnStatus,"Logo is not displayed");
-
-       //Logo is displayed
-        blnStatus =driver.findElement(By.xpath("//a[@title='Request Demo' and contains(@class,'coh-link utility-nav-link coh-style-solid-orange-button')]")).isEnabled();
-        if(blnStatus){
-            System.out.println("Able to see the automationanywhere 'Request Demo' button enabled");
-        }else{
-            System.out.println("Not Able to see the automationanywhere 'Request Demo' button not enabled");
-        }
-//        Assert.assertTrue("Logo is not displayed",blnStatus);
-        Assertions.assertTrue(blnStatus,"Logo is not displayed");
-
-
-//        //Navigate to correct page
-//        String[] linkToClick=new String[]{"Products","Solutions","Resources","Beyond RPA","Company"};
-//        String[] pageTitle=new String[]{"Scale Your Business with Enterprise Automation","Tailored Automation Solutions",
-//                "Automation Resources","Optimize Efficiency with Business Process Automation","Intelligent Automation Companies"};
-//
-//                List<WebElement> webElements = driver.findElements(By.xpath("//div[contains(@class,'coh-column primary-menu')]/ul/li/a"));
-//       for(int i=0; i<webElements.size();i++){
-//           System.out.println("Link Displayed :- "+webElements.get(i).getAttribute("text"));
-//           if(webElements.get(i).getAttribute("text").equalsIgnoreCase(linkToClick[i])){
-//               System.out.println("Menu Item displayed");
-//           }else{
-//               System.out.println("Menu Not Item displayed");
-//           }
-//       }
-//
-//
-//
-//
-//        for(int i=0;i<linkToClick.length;i++){
-//            System.out.println("Navigating to page:-"+linkToClick[i]);
-//            driver.findElement(By.xpath("//div[contains(@class,'coh-column primary-menu')]/ul/li/a[contains(text(),'"+linkToClick[i]+"')]")).click();
-//
-//            System.out.println("Page Title:-"+driver.getTitle());
-//            if(driver.getTitle().contains(pageTitle[i])){
-//                System.out.println("Page Title matched:- Actual Page Title :-"+driver.getTitle() +" Expected Page Title :-"+pageTitle[i]);
-//            }else{
-//                System.out.println("Page Title not matched:- Actual Page Title :-"+driver.getTitle() +" Expected Page Title :-"+pageTitle[i]);
-//            }
-//        }
-//
-
-//       driver.quit();
 
     }
 
         @Test
         public void verifyMenuItemsDisplayedAndAbleToNavigatetoExpectedPageTestCase() {
-            //Navigate to correct page
-            String[] linkToClick=new String[]{"Products","Solutions","Resources","Beyond RPA","Company"};
-            String[] pageTitle=new String[]{"Scale Your Business with Enterprise Automation","Tailored Automation Solutions",
-                    "Automation Resources","Optimize Efficiency with Business Process Automation","Intelligent Automation Companies"};
-            WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(90));
-             wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class,'coh-column primary-menu')]/ul/li/a")));
-            //Menu Items Displayed
-            List<WebElement> webElements = driver.findElements(By.xpath("//div[contains(@class,'coh-column primary-menu')]/ul/li/a"));
-            for(int i=0; i<webElements.size();i++){
-                System.out.println("Link Displayed :- "+webElements.get(i).getAttribute("text"));
-                if(webElements.get(i).getAttribute("text").equalsIgnoreCase(linkToClick[i])){
-                    System.out.println("Menu Item displayed");
-                }else{
-                    System.out.println("Menu Not Item displayed");
+            try{
+                //Navigate to correct page
+                String[] linkToClick=new String[]{"Products","Solutions","Resources","Beyond RPA","Company"};
+                String[] pageTitle=new String[]{"Scale Your Business with Enterprise Automation","Tailored Automation Solutions",
+                        "Automation Resources","Optimize Efficiency with Business Process Automation","Intelligent Automation Companies"};
+                WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(90));
+                wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class,'coh-column primary-menu')]/ul/li/a")));
+                //Menu Items Displayed
+                List<WebElement> webElements = driver.findElements(By.xpath("//div[contains(@class,'coh-column primary-menu')]/ul/li/a"));
+                for(int i=0; i<webElements.size();i++){
+//                    System.out.println("Link Displayed :- "+webElements.get(i).getAttribute("text"));
+                    if(webElements.get(i).getAttribute("text").equalsIgnoreCase(linkToClick[i])){
+                        System.out.println("Menu Item displayed:- "+webElements.get(i).getAttribute("text"));
+                    }else{
+                        System.out.println("Menu Not Item displayed:- "+webElements.get(i).getAttribute("text"));
+                    }
+                    boolean blnStatusMenuItemDisplayed=webElements.get(i).getAttribute("text").equalsIgnoreCase(linkToClick[i]);
+                    Assertions.assertTrue(blnStatusMenuItemDisplayed,"Menu Item:-'"+linkToClick[i]+"' is not displayed");
                 }
-                boolean blnStatusMenuItemDisplayed=webElements.get(i).getAttribute("text").equalsIgnoreCase(linkToClick[i]);
-//                Assert.assertTrue("Menu Item:-'"+linkToClick[i]+"' is not displayed",blnStatusMenuItemDisplayed);
-                Assertions.assertTrue(blnStatusMenuItemDisplayed,"Menu Item:-'"+linkToClick[i]+"' is not displayed");
-            }
 
 
 
-            //Able to click menu item and able to navigate to new page and able to see expected page title displayed
-            for(int i=0;i<linkToClick.length;i++){
-                System.out.println("Navigating to page:-"+linkToClick[i]);
-                driver.findElement(By.xpath("//div[contains(@class,'coh-column primary-menu')]/ul/li/a[contains(text(),'"+linkToClick[i]+"')]")).click();
+                //Able to click menu item and able to navigate to new page and able to see expected page title displayed
+                for(int i=0;i<linkToClick.length;i++){
+                    System.out.println("Navigating to page:-"+linkToClick[i]);
+                    driver.findElement(By.xpath("//div[contains(@class,'coh-column primary-menu')]/ul/li/a[contains(text(),'"+linkToClick[i]+"')]")).click();
 
-                System.out.println("Page Title:-"+driver.getTitle());
-                if(driver.getTitle().contains(pageTitle[i])){
-                    System.out.println("Page Title matched:- Actual Page Title :-"+driver.getTitle() +" Expected Page Title :-"+pageTitle[i]);
-                }else{
-                    System.out.println("Page Title not matched:- Actual Page Title :-"+driver.getTitle() +" Expected Page Title :-"+pageTitle[i]);
+//                    System.out.println("Page Title:-"+driver.getTitle());
+                    if(driver.getTitle().contains(pageTitle[i])){
+                        System.out.println("Page Title matched:- Actual Page Title :-"+driver.getTitle() +" Expected Page Title :-"+pageTitle[i]);
+                    }else{
+                        System.out.println("Page Title not matched:- Actual Page Title :-"+driver.getTitle() +" Expected Page Title :-"+pageTitle[i]);
+                    }
+                    boolean blnPageTitleDisplayed=driver.getTitle().contains(pageTitle[i]);
+                    Assertions.assertTrue(blnPageTitleDisplayed,"Not navigated to expected page:-"+pageTitle[i]+" on clicking menu item:-'"+linkToClick[i]+"' ");
+
                 }
-                boolean blnPageTitleDisplayed=driver.getTitle().contains(pageTitle[i]);
-//                Assert.assertTrue("Not navigated to expected page:-"+pageTitle[i]+" on clicking menu item:-'"+linkToClick[i]+"' ",blnPageTitleDisplayed);
-                Assertions.assertTrue(blnPageTitleDisplayed,"Not navigated to expected page:-"+pageTitle[i]+" on clicking menu item:-'"+linkToClick[i]+"' ");
 
+                driver.quit();
+
+            }catch (Exception e){
+                Assertions.fail("verifyMenuItemsDisplayedAndAbleToNavigatetoExpectedPageTestCase :- Exception :- "+e);
             }
-
-            driver.quit();
         }
 
 
